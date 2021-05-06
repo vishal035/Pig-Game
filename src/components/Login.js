6878416eb8cdc458155daa57cc79f92e0dff926d
vishal035 <kumarvishal880288@gmai;.com>
2021-05-06 12:37:58 +0530

    Login Component redesigned

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,5 +1,6 @@
 import React from 'react';
-import './Login.css';
+// import './Login.css';
+import './login-1.css';
 
 const Login = (props) => {
   const {
@@ -17,9 +18,69 @@ const Login = (props) => {
   return (
     <div className="Login">
       <div className="login-container">
-        <p className="log">Login</p>
+        <main>
+          <form>
+            <label
+              htmlFor="email"
+              className="email text-edit mr-bottom input-label"
+            >
+              Email Address
+            </label>
+            <input
+              type="email"
+              name="email"
+              id="emailInput"
+              className="mr-bottom inp-1 text-edit"
+              placeholder="&ldquo;somthing&commat;domain.com&rdquo; "
+              required
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+            />
+            <p className="errorMsg">{emailerror}</p>
+            <label
+              htmlFor="password"
+              className="password mr-bottom text-edit input-label"
+            >
+              Password
+            </label>
+            <input
+              required
+              type="password"
+              name="password"
+              id="passwordInput"
+              className="mr-bottom pass-inot-1 text-edit"
+              minLength="8"
+              placeholder="Secret Phrase"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+            />
+            {/* <button class="btn">Sign up</button> */}
+            {hasAcc ? (
+              <>
+                <button onClick={handleLogin} className="btn">
+                  Sign in
+                </button>
 
-        <br></br>
+                <p className="p1">
+                  Don't have an account ?{' '}
+                  <span onClick={() => setHasAcc(!hasAcc)}>Sign up</span>
+                </p>
+              </>
+            ) : (
+              <>
+                <button onClick={handleSignup} className="btn">
+                  Sign up
+                </button>
+                <p className="p1">
+                  Have an acccount ?
+                  <span onClick={() => setHasAcc(!hasAcc)}> Sign in</span>
+                </p>
+              </>
+            )}
+          </form>
+        </main>
+
+        {/* <br></br>
 
         <label className="user">Username</label>
         <br></br>
@@ -74,7 +135,7 @@ const Login = (props) => {
               </p>
             </>
           )}
-        </div>
+        </div> */}
       </div>
     </div>
   );
